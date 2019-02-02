4339ee5dbb94ed887307b47ce8dab35cae3e3b88
Sibeswar Bhaumik <sombhaumik@yahoo.co.in>
2019-02-02 13:49:18 +0530

    First Commit..

---
+++ TaxService/src/main/java/com/javaonlyjava/TaxService/TaxServiceApplication.java
@@ -0,0 +1,14 @@
+package com.javaonlyjava.TaxService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxServiceApplication.class, args);
+	}
+
+}
+
